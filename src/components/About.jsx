1bdfdaa7d79d5b605aedb9efba1d54a53ae80efd
KusMar00 <markusraunjohansen@gmail.com>
2024-01-22 14:55:53 +0100

    Add text for about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,6 +5,13 @@ const About = () => (
     <h2 className="font-poppins text-white text-5xl font-semibold">
       About<span className="text-secondary">.</span>
     </h2>
+    <p className="font-poppins text-white text-l mt-10">
+      In my free time i enjoy spending time working on small side projects, like
+      this portfolio and other fun stuff. When i'm not writing code i like to go
+      for a run, throw plastic discs at baskets (play disc golf) or make coffee
+      in complicated ways. <br /> <br />I am also a huge music nerd, and i try
+      to listen as much as possible. I'm currently listening to...
+    </p>
   </section>
 );
 export default About;
