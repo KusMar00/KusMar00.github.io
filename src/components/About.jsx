d95925467dba759816e3645b5d4860138f54b76e
KusMar00 <markusraunjohansen@gmail.com>
2024-01-22 15:37:50 +0100

    Update text for about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,17 +1,29 @@
 import styles from "../styles";
+import { useState, useEffect } from "react";
 
-const About = () => (
-  <section id="about" className={`${styles.paddingY}`}>
-    <h2 className="font-poppins text-white text-5xl font-semibold">
-      About<span className="text-secondary">.</span>
-    </h2>
-    <p className="font-poppins text-white text-l mt-10">
-      In my free time i enjoy spending time working on small side projects, like
-      this portfolio and other fun stuff. When i'm not writing code i like to go
-      for a run, throw plastic discs at baskets (play disc golf) or make coffee
-      in complicated ways. <br /> <br />I am also a huge music nerd, and i try
-      to listen as much as possible. I'm currently listening to...
-    </p>
-  </section>
-);
+const About = () => {
+  const [data, setData] = useState();
+  const getSpotifyData = () => {};
+
+  return (
+    <section id="about" className={`${styles.paddingY}`}>
+      <h2 className="font-poppins text-white text-5xl font-semibold">
+        About<span className="text-secondary">.</span>
+      </h2>
+      <p className="font-poppins text-white text-xl mt-10">
+        I'm 23 years old living in Copenhagen . I recently graduated from the
+        IT-Univeristy of Copenhagen with a BSc in Software Development, and am
+        now looking for a workplace where i can apply my skills. I dream of
+        working as a full-stack web developer but i also have a keen eye for
+        UI/Frontend work. <br /> <br />
+        In my free time i enjoy spending time working on small side projects,
+        like this portfolio and other fun stuff. When i'm not writing code i
+        like to go for a run, throw plastic discs at baskets (play disc golf) or
+        make coffee in complicated ways (Espresso, V60). <br /> <br />I am also
+        a huge music nerd, and i try to listen as much as possible. I'm
+        currently listening to...
+      </p>
+    </section>
+  );
+};
 export default About;
